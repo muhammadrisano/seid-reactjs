6c660b6a5fcd8b238ff20aaf5378949be73be03b
Muhammad Risano Akbar <muhammadrisano@gmail.com>
2019-12-14 09:58:23 +0700

    risano

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,31 +1,32 @@
 import React from 'react';
 import './navbar.css';
+import {Link} from 'react-router-dom'
 
 const Navbar =()=>{
   return(
     <nav className="navbar navbar-expand-lg navbar-light navbar-costume fixed-top">
     <div className="container">
-      <a className="navbar-brand" href="/">
+      <Link className="navbar-brand" to="/">
         <img src={require('../../assets/img/logo.svg')} alt="" width="80"/>
-        </a>
+        </Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
-            <a className="nav-link" href="/costumer">Costumer</a>
+            <Link className="nav-link" to="/costumer">Costumer</Link>
           </li>
           <li>
-            <a href="/talent" className="nav-link">Talent</a>
+            <Link href="/talent" className="nav-link">Talent</Link>
           </li>
         </ul>
           <ul className="navbar-nav">
             <li>
-              <a href="/regcostum" className="nav-link">Daftar Costumer</a>
+              <Link to="/regcostum" className="nav-link">Daftar Costumer</Link>
             </li>
           <li>
-            <a href="/register" className="btn btn-warning">Daftar Talent</a>
+            <Link to="/register" className="btn btn-warning">Daftar Talent</Link>
           </li>
             
         </ul>
